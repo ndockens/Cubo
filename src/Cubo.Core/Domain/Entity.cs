c7b1e4c7c4d84f515c8e78c206ed20f4dab91672
Nathan Dockens <Nathan@nathans-mbp.attlocal.net>
2019-11-19 22:04:47 -0600

    Added initial domain models.

---
+++ src/Cubo.Core/Domain/Entity.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Cubo.Core.Domain
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ src/Cubo.Core/Domain/Item.cs
@@ -0,0 +1,19 @@
+namespace Cubo.Core.Domain
+{
+    public class Item : Entity
+    {
+        public string Key { get; protected set; }
+        public string Value { get; protected set; }
+
+        protected Item()
+        {
+
+        }
+
+        public Item(string key, string value)
+        {
+            Key = key.ToLowerInvariant();
+            Value = value;
+        }
+    }
+}
